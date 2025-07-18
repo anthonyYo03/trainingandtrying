cd49afd6b9a363b7400e53105e00feb5f8aad644
anthonyYo03 <antoyou78@gmail.com>
2025-07-18 09:10:51 +0300

    Second Change Front-end Backend

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ fetch('https://trying-backend.onrender.com/try').then(res=>res.json()).then(data
 {
 
 for (let i = 0; i<= data.length; i++) {
-  excelData.innerHTML=`Id: ${data[i].Id} Name : ${data[i].Name}`
+  const li=document.createElement("li");
+  li.innerHTML=`Id: ${data[i].Id} Name : ${data[i].Name}`
+  excelData.appendChild(li);
     }
 
 
